744d9e46def7cac29cf12d42a1541786671daa4d
Vitória <vitoriakamila585@gmail.com>
2025-10-07 02:10:16 -0300

    atualizado

--- backend/server.js
+++ backend/server.js
@@ -3,12 +3,15 @@ import cors from "cors";
 import dotenv from "dotenv";
 import { GoogleGenerativeAI } from "@google/generative-ai";
 
+
+
 dotenv.config();
 const app = express();
 const port = 5000;
-
 app.use(cors());
 app.use(express.json());
+
+
 
 // Gemini
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
@@ -17,32 +20,33 @@ const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
 
 app.post("/chat", async (req, res) => {
-  const { message } = req.body;
+  const { message } = req.body;
+  if (!message) {
+    return res.status(400).json({ reply: "Mensagem vazia." });
+  }
 
-  if (!message) {
-    return res.status(400).json({ reply: "Mensagem vazia." });
-  }
+  try {
+    const completion = await model.generateContent({
+      contents: [
+        {
+          parts: [
+            { text: "Você é um assistente especializado em monitoramento inteligente e eficiência energética. Responda de forma clara e didática." },
+            { text: message }
+          ]
+        }
+      ]
+    });
 
-  try {
-    const completion = await model.generateContent({
-      contents: [
-        {
-          parts: [
-            { text: "Você é um assistente especializado em monitoramento inteligente e eficiência energética. Responda de forma clara e didática." },
-            { text: message }
-          ]
-        }
-      ]
-    });
 
-    const reply = completion.response.candidates[0].content.parts[0].text;
-    res.json({ reply });
-  } catch (error) {
-    console.error("Erro no Gemini:", error);
-    res.status(500).json({ reply: "Erro ao se comunicar com o Gemini." });
-  }
+
+    const reply = completion.response.candidates[0].content.parts[0].text;
+    res.json({ reply });
+  } catch (error) {
+    console.error("Erro no Gemini:", error);
+    res.status(500).json({ reply: "Erro ao se comunicar com o Gemini." });
+  }
 });
 
 app.listen(port, () => {
-  console.log(`Servidor rodando em http://localhost:${port}`);
+  console.log(`Servidor rodando em http://localhost:${port}`);
 });
